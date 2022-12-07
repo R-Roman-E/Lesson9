5c84cefe30efaa6c6d219f5c9b6a2a68faa34cbb
Roman <rom_ich@mail.ru>
2022-12-07 17:52:58 +0300

    AllTask

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+void PrintSumm(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (n<=m)
+    {
+        Console.Write($"Сумма элементов {summ} ");
+        return;
+    }
+    PrintSumm(m, n-1, summ);
+}
+
+int userNumberM = GetNumber("Введите число M:");
+int userNumberN = GetNumber("Введите число N:");
+PrintSumm(userNumberM, userNumberN, 0);
---
+++ Task68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+int Akkerman(int M, int N)
+{
+  if (M == 0) return N + 1;
+  else if (N == 0) return Akkerman(M - 1, 1);
+  else return Akkerman(M - 1, Akkerman(M, N - 1));
+}
+int userNumberM = GetNumber("Введите число M:");
+int userNumberN = GetNumber("Введите число N:");
+int function = Akkerman(userNumberM, userNumberN);
+Console.Write($"A(M,N) = {function} ");
